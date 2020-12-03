ce26f2fe8d95274d4cf2e6a981e0a1216213d75a
hassy8411 <runninglong6@gmail.com>
2020-12-03 16:57:52 +0000

    Fix

---
+++ kadai-02/index.js
@@ -0,0 +1,11 @@
+const firstname ='Tanaka'
+const familyname ='Makiko'
+console.log(firstname+familyname)
+
+console.log(1+2+3+4+5+6+7+8+9+10)
+
+const dog=false
+const cat=true
+console.log(dog||cat)
+console.log(dog&&cat)
+console.log(!dog&&!cat)
